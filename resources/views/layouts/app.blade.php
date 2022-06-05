237c1a30960b8119bb12f9717025bffedf0c6bc1
Mony0775 <phoeunmony12@gmail.com>
2022-06-05 21:39:40 +0700

    TP15

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+// resource/views/layouts/app.blade.php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel QuickStart - Basic</title>
+    <!-- CSS AND JavaScript -->
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <!-- NavBar Contents -->
+        </nav>
+    </div>
+
+    @yield('content')
+</body>
+</html>
